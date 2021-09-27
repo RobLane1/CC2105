f3de9477dbda9b3ce315c0c8d8025cbbbcec1f8d
RobLane1 <Robert.lane@hcl.com>
2021-09-27 17:35:47 +0530

    added stable branch

---
+++ js-storefront/spartacusb2bstore/src/app/spartacus/spartacus-configuration.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { translationChunksConfig, translations } from "@spartacus/assets";
+import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, SiteContextConfig } from '@spartacus/core';
+import { defaultCmsContentProviders, layoutConfig, mediaConfig } from "@spartacus/storefront";
+import { defaultB2bCheckoutConfig, defaultB2bOccConfig } from "@spartacus/setup";
+import { environment } from 'src/environments/environment';
+
+const occConfig: OccConfig = { backend: { occ: {} } };
+// only provide the `occ.baseUrl` key if it is explicitly configured, otherwise the value of
+// <meta name="occ-backend-base-url" > is ignored.
+// This in turn breaks the deployment in CCv2.
+// https://github.com/SAP/spartacus/issues/5886
+occConfig.backend.occ.prefix = '/occ/v2/'
+if (environment.occBaseUrl) {
+  occConfig.backend.occ.baseUrl = environment.occBaseUrl;
+}
+
+@NgModule({
+  declarations: [],
+  imports: [
+  ],
+  providers: [provideConfig(layoutConfig), provideConfig(mediaConfig), ...defaultCmsContentProviders, provideConfig({
+    backend: occConfig.backend,
+    smartEdit: {
+      storefrontPreviewRoute: 'cx-preview',
+      //TODO: adjust origins as necessary
+      allowOrigin: 'api.cmxuwm6npz-hclameric3-d1-public.model-t.cc.commerce.ondemand.com, *.*.model-t.cc.commerce.ondemand.com:443',
+    },
+    context: {
+      urlParameters: ['baseSite', 'language', 'currency'],
+      baseSite: ['powertools-spa'],
+      currency: ['USD'],
+      language: ['en'],
+    },
+    i18n: {
+      resources: translations,
+      chunks: translationChunksConfig,
+      fallbackLang: 'en'
+    },
+    features: {
+      level: '3.2'
+    }
+  }), provideConfig(<SiteContextConfig>{
+    context: {
+      currency: ['USD'],
+      language: ['en'],
+    },
+  }), provideConfig(<I18nConfig>{
+    i18n: {
+      resources: translations,
+      chunks: translationChunksConfig,
+      fallbackLang: 'en'
+    },
+  }), provideConfig(<FeaturesConfig>{
+    features: {
+      level: '3.4'
+    }
+  }), provideConfig(defaultB2bOccConfig), provideConfig(defaultB2bCheckoutConfig)]
+})
+export class SpartacusConfigurationModule { }
